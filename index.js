aadaeb190abdd361abad62f8a3cc7e7a07570717
NotFaizen <83504931+NotFaizen@users.noreply.github.com>
2021-09-29 09:46:56 +0000

    removed keepalive, added homepage and /hug endpoint

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 const express = require("express")
 const app = express()
-const ms = require('ms')
-const keepAlive = require('keep-alive')
-const base_url = "https://gud-api.gofaizen.repl.co"
 
-keepAlive(base_url, ms('5m')) // this is for keeping the API from dying
+
 require("./routes")(app)
 
 app.get("/", (req, res) => {
-  res.send("Home page") // endpoint list soonTM
+  res.send({
+  "base_url": "https://gud-api.gofaizen.repl.co",
+  "discord_server": "Soon™ & join https://discord.gg/HbWpEAcmFk",
+  "twitter": "https://twitter.com/TheRealFaizen",
+  "endpoints": {
+    "fun_endpoints": [
+      "GET /fun/dadjoke",
+      "GET /fun/pickuplines",
+    ],
+    "interaction_endpoints": [
+      "GET /interactions/hug"
+    ],
+    "nsfw_endpoints": [
+      "Later, I am lazy"
+    ]
+  }
+  }) // endpoint list soonTM
 })
 
 app.listen(5000, () => {
   console.log("API is up and running, I think")
 })
+app.set('json spaces', 1);
